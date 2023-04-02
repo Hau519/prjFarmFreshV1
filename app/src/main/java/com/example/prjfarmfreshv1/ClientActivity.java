25f67dccbfbd6c833e96f9845b110a10f323d4b8
Hau Vu <65142251+Hau519@users.noreply.github.com>
2023-04-02 18:30:30 -0400

    finish client activity (not using navbar for profile)

---
+++ app/src/main/java/com/example/prjfarmfreshv1/ClientActivity.java
@@ -0,0 +1,84 @@
+package com.example.prjfarmfreshv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.prjfarmfreshv1.models.User;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ClientActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView tvName, tvEmail;
+    Button btnUpdate, btnShop;
+
+    User user;
+
+    DatabaseReference usersTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+        initialize();
+        fillInformation();
+    }
+
+    private void fillInformation() {
+        user = (User)getIntent().getExtras().getSerializable("user");
+        tvName.setText(user.getName());
+        tvEmail.setText(user.getEmail());
+    }
+
+    private void initialize() {
+        tvName = findViewById(R.id.tvName);
+        tvName.setEnabled(false);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvEmail.setEnabled(false);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnShop = findViewById(R.id.btnShopNow);
+        btnShop.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        usersTable= FirebaseDatabase.getInstance().getReference("Users");
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.btnShopNow:
+                Intent i = new Intent(this, MainActivity.class); //TODO : change to product activity when Luke's done
+                i.putExtra("user", user);
+                startActivity(i);
+                break;
+            case R.id.btnUpdate:
+                try{
+                    if (btnUpdate.getText().toString().equals("Update")){
+                        tvEmail.setEnabled(true);
+                        tvName.setEnabled(true);
+                        btnUpdate.setText("Save");
+                    }else if(btnUpdate.getText().toString().equals("Save")){
+                        String email = tvEmail.getText().toString();
+                        String name = tvName.getText().toString();
+                        User updateUser = new User(user.getId(), name, email, user.getPassword());
+                        usersTable.child(String.valueOf(user.getId())).setValue(updateUser);
+                        Snackbar.make(view, "Your information is updated succesffully",
+                                Snackbar.LENGTH_LONG).show();
+                        tvEmail.setEnabled(false);
+                        tvName.setEnabled(false);
+                        btnUpdate.setText("Update");
+                    }
+                }catch(Exception ex){
+                    Snackbar.make(view, ex.getMessage(),
+                            Snackbar.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+}
