34a5fd32244c745c91bc32f0110b8f5565a38958
Hau Vu <65142251+Hau519@users.noreply.github.com>
2023-04-04 07:14:40 -0400

    order

--- app/src/main/java/com/example/prjfarmfreshv1/OrderListActivity.java
+++ app/src/main/java/com/example/prjfarmfreshv1/OrderListActivity.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.prjfarmfreshv1.models.Order;
 import com.example.prjfarmfreshv1.models.User;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,9 +24,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class OrderListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class OrderListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
 
     ListView lvOrders;
+    ImageView ivAccount;
+    Button btnShop;
     User user;
     ArrayList<Order> orderList;
 
@@ -41,10 +46,13 @@ public class OrderListActivity extends AppCompatActivity implements AdapterView.
     private void initialize() {
         lvOrders = findViewById(R.id.lvOrders);
         lvOrders.setOnItemClickListener(this);
+        ivAccount = findViewById(R.id.ivAccount);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Orders");
         arrayAdapter = new ArrayAdapter<>(this, R.layout.one_item, orderList);
         user = (User)getIntent().getExtras().getSerializable("user");
+        ivAccount.setOnClickListener(this);
+        btnShop = findViewById(R.id.btnShop);
         readOrderData();
 //        orderAdapter = new ArrayAdapter<>(this, R.layout.O, orderList)
     }
@@ -87,5 +95,20 @@ public class OrderListActivity extends AppCompatActivity implements AdapterView.
         intent2.putExtra("order", order);
         intent2.putExtra("user", user);
         startActivity(intent2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btnShop:
+                Intent i = new Intent(this, MainActivity.class); //TODO : change to product activity when Luke's done
+                i.putExtra("user", user);
+                startActivity(i);
+                break;
+            case R.id.ivAccount:
+                finish();
+                break;
+        }
     }
 }
