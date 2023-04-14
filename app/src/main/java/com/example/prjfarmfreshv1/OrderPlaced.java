949a720686e892c72979d08cca7ff598fae99caf
Xiaoan93 <laoxiaoan@gmail.com>
2023-04-13 22:25:58 -0400

    April 13 commit

--- app/src/main/java/com/example/prjfarmfreshv1/OrderPlaced.java
+++ app/src/main/java/com/example/prjfarmfreshv1/OrderPlaced.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class OrderPlaced extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_placed);
+
+        String orderId = UUID.randomUUID().toString();
     }
 }
